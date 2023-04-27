1533d842c50f62fd7e4717bf9cf28b45df1bc103
freitaspedro <pfreitas@id.uff.br>
2023-04-27 19:04:41 -0300

    add sounds and buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,60 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  static var colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.cyan,
+    Colors.blue,
+    Colors.purple
+  ];
+
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Widget buildKey(int i) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: colors[i],
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.zero)
+            ),
+        ),
+        onPressed: () {
+          playSound(i + 1);
+        },
+        child: SizedBox.shrink(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(0),
+              buildKey(1),
+              buildKey(2),
+              buildKey(3),
+              buildKey(4),
+              buildKey(5),
+              buildKey(6),
+            ],
+          ),
         ),
       ),
     );
